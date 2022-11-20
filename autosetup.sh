f5aa2d964c4c11fc7ab688da441991ccc448e63a
Nuno Dias <jookaada@protonmail.com>
2022-11-20 18:19:41 +0000

    Update autosetup.sh

--- autosetup.sh
+++ autosetup.sh
@@ -29,7 +29,7 @@ curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update && sudo apt-get install -y spotify-client
 echo -e "${c}Final Touches..."; $r
-sudo apt install -y gnome-tweaks terminator
+sudo apt install -y gnome-tweaks terminator gnome-shell-extension-manager
 curl -O https://raw.githubusercontent.com/bb010g/betterdiscordctl/master/betterdiscordctl
 chmod +x betterdiscordctl
 sudo mv betterdiscordctl /usr/local/bin
