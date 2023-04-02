6567882e615ff632bdac769fc37f672bc2027d72
Nuno Dias <jookaada@protonmail.com>
2023-04-02 12:51:48 +0100

    Update autosetup.sh

--- autosetup.sh
+++ autosetup.sh
@@ -29,13 +29,8 @@ wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt update && sudo apt install -y code
 echo -e "${c}Installing Spotify..."; $r
-curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
+curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update && sudo apt-get install -y spotify-client
 echo -e "${c}Final Touches..."; $r
-sudo apt install -y gnome-tweaks gnome-shell-extension-manager vim
-curl -O https://raw.githubusercontent.com/bb010g/betterdiscordctl/master/betterdiscordctl
-chmod +x betterdiscordctl
-sudo mv betterdiscordctl /usr/local/bin
-sudo betterdiscordctl self-upgrade
-betterdiscordctl install
+sudo apt install -y vim
